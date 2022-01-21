e45bfc39114f544a1040c67b577d951972761ad8
RennyG <rennygorozabel@hotmail.com>
2022-01-21 18:30:58 -0500

    REDUX * Handle LogIn / LogOut * Integración con backEnd * HomePage

--- src/App.js
+++ src/App.js
@@ -4,13 +4,19 @@ import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import './App.css';
 import LogIn from './pages/LogIn';
+import { selectUser } from './features/userSlice';
+import Home from './pages/Home';
+import React from 'react';
+import { useSelector } from 'react-redux';
+
 
 
 function App() {
+  const user = useSelector(selectUser);
   return (
-    <div className="App">
-        <LogIn/>
-    </div>
+      <React.Fragment>
+          {user ? <Home user={user}/> : <LogIn/>}
+      </React.Fragment>
   );
 }
 
